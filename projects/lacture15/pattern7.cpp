a83abeb3b76385162d7686aae918b09ae7fc011d
unknown <amitraikwar993613@gmail.com>
2026-02-04 15:41:00 +0530

    notes

---
+++ projects/lacture15/pattern7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int fibbo(int g)
+{
+    // base case
+    if (g == 0 or g == 1)
+    {
+        return g;
+    }
+    return fibbo(g - 1) + fibbo(g - 2);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int g = 0;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << fibbo(g) << "  ";
+            g++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
