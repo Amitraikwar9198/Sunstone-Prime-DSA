d8f8af483b719763f46b40fb139135e5674e15f0
unknown <amitraikwar993613@gmail.com>
2026-02-04 16:19:19 +0530

    notes

--- projects/lacture15/decimal_to_binary.cpp
+++ projects/lacture15/decimal_to_binary.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int rev(int n){
+int rev(int n)
+{
     int rev = 0;
-    while(n>0){
-        rev = rev*10 + n%10;
-        n = n/10;
+    while (n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n = n / 10;
     }
     return rev;
 }
-int main(){
-int bn = 1;
-int n;
-cin>>n;
-while(n>0){
-int k = n%2;
-bn = bn*10+k;
-n = n/2;
-
-}
-cout<<rev(bn)/10<<endl;
-return 0;
-
+int main()
+{
+    int bn = 1;
+    int n;
+    cin >> n;
+    while (n > 0)
+    {
+        int k = n % 2;
+        bn = bn * 10 + k;
+        n = n / 2;
+    }
+    cout << rev(bn) / 10 << endl;
+    return 0;
 }
--- projects/lacture15/fibbonacci.cpp
+++ projects/lacture15/fibbonacci.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fibbo(int g){
-//base case
-if(g==0 or g==1){
-return g;
+int fibbo(int g)
+{
+    // base case
+    if (g == 0 or g == 1)
+    {
+        return g;
+    }
+    return fibbo(g - 1) + fibbo(g - 2);
 }
-return fibbo(g-1)+fibbo(g-2);
-
-}
-int main(){
-int n;
-cin>>n;
-cout<<fibbo(n)<<endl;
-return 0;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibbo(n) << endl;
+    return 0;
 }
