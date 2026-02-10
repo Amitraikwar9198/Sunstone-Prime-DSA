4186b5da34a443e61d9352fc67fb509a51f3f431
unknown <amitraikwar993613@gmail.com>
2026-02-10 14:04:01 +0530

    notes

--- projects/lacture18/check_args_position.cpp
+++ projects/lacture18/check_args_position.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={10,50,12,15,16};
+    
     int n;
     cin>>n;
+    int arr[n];
+    for(int i=0 ; i<5; i++){
+        cin>>arr[i];
+    }
+    int value=20;
     
     for(int i=0;i<5;i++){
         
-         if(arr[i]==n){
+         if(arr[i]==value){
         cout<<"index is "<<i+1<<endl;
         break;
         
