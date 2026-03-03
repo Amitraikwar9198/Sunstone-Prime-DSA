1bc8626cf90bbaa1fc02112c53c35da3657634f1
unknown <amitraikwar993613@gmail.com>
2026-03-03 21:19:14 +0530

    basic code

--- projects/pr.cpp
+++ projects/pr.cpp
@@ -4,11 +4,18 @@ int add(int a,int b){
     int sum = a+b;
     return sum;
 }
+int subtract(int c, int d){
+    int sub=c-d;
+    return sub;
+}
 int main(){
-    int a,b;
+    int a,b,c,d;
     cout<<"Enter any two numbers : ";
-    cin>>a>>b;
+    cin>>a>>b>>c>>d;
     int result = add(a , b);
+    int result1 = subtract(c,d);
+
     cout<<"sum : "<<result;
+    cout<<"\nSubtract : "<<result1;
     return 0;
 }
