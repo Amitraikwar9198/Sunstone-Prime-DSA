6212ebcaffd39543bca992a5221fb8062ff3baa9
unknown <amitraikwar993613@gmail.com>
2026-04-16 23:31:54 +0530

    leetcode

---
+++ projects/leetcode/532.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if (k < 0)
+            return 0;
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int i = 0, j = 1;
+        int count = 0;
+
+        while (j < n) {
+            if (i == j || nums[j] - nums[i] < k) {
+                j++;
+            } 
+            else if (nums[j] - nums[i] > k) {
+                i++;
+            } 
+            else {
+                count++;
+                i++;
+                j++;
+
+                // Skip duplicates
+                while (j < n && nums[j] == nums[j - 1])
+                    j++;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {3, 1, 4, 1, 5};
+    int k = 2;
+
+    int result = obj.findPairs(nums, k);
+
+    cout << "Number of unique k-diff pairs: " << result << endl;
+
+    return 0;
+}
