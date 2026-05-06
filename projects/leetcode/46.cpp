37c7f65f2b1195aeb85090603f3de2a479229e67
unknown <amitraikwar993613@gmail.com>
2026-05-06 21:09:40 +0530

    leetcode

---
+++ projects/leetcode/46.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> ans;
+
+    sort(nums.begin(), nums.end());
+    ans.push_back(nums);
+
+    while(next_permutation(nums.begin(), nums.end())) {
+        ans.push_back(nums);
+    }
+
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) cin >> nums[i];
+
+    vector<vector<int>> result = permute(nums);
+
+    for(auto &v : result) {
+        for(int x : v) cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
