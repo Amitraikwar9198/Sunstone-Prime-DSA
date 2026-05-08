e207a0db41a035c58fdd38cbd27ff295da731641
unknown <amitraikwar993613@gmail.com>
2026-05-08 22:55:13 +0530

    leetcode

---
+++ projects/leetcode/48.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    // Transpose matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Reverse each row
+    for(int i = 0; i < n; i++) {
+        int j = 0, k = m - 1;
+
+        while(j < k) {
+            swap(matrix[i][j], matrix[i][k]);
+            j++;
+            k--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    rotate(matrix);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
